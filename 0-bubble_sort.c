4beb97e11d88a4af9ae4573a1a6fadb392128602
root <root@ABU-ABDILLAH.localdomain>
2023-01-22 14:22:27 +0100

    Fixed bubble sort order error

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -35,10 +35,13 @@ void bubble_sort(int *array, size_t size)
 		while (i < size && array[i - 1] < array[i])
 			i++;
 		j = i;
-		while (j < size && array[j - 1] > array[j])
+		while (j < size)
 		{
-			swap_array(array, j - 1, j);
-			print_array(array, size);
+			if (array[j - 1] > array[j])
+			{
+				swap_array(array, j - 1, j);
+				print_array(array, size);
+			}
 			j++;
 		}
 	}
